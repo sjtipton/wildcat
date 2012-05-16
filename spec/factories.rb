92e4ce3bcafe2dd079bf5a1ddd54d0d73737dd6b
Steve Tipton <steve.tipton@employmentguide.com>
2012-05-16 16:16:24 -0400

    refactor factories not to use override of forgery

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,18 +1,18 @@
 FactoryGirl.define do
 
   factory :team, class: Wildcat::Team do
-    name          { Forgery.dictionaries[:team_names].sample }
+    name          { ["Baltimore Ravens", "Carolina Panthers"].sample }
     nickname      { name.split(' ').last }
-    abbreviation  { Forgery.dictionaries[:team_abbreviations].sample }
+    abbreviation  { ["BAL", "CAR"].sample }
     location      { "#{Forgery(:address).city}, #{Forgery(:address).state_abbrev}" }
     conference    { %w(AFC NFC).sample }
     division      { %w(North South East West).sample }
   end
 
   factory :game, class: Wildcat::Game do
-    label         { "#{Forgery.dictionaries[:team_names].sample.split(' ').last} at #{Forgery.dictionaries[:team_names].sample.split(' ').last}" }
+    label         { "Ravens at Panthers" }
     season        { Forgery(:date).year }
-    stadium       { "#{Forgery.dictionaries[:team_names].sample.split(' ').last} #{%w(Field Stadium Park Dome Coliseum).sample}" }
+    stadium       { "Bank of America Stadium" }
     week          nil
     home_team_id  SecureRandom.random_number(1e2.to_i)
     away_team_id  SecureRandom.random_number(1e2.to_i)
