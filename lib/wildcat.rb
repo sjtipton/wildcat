8bfd4520917bf8b7da208c1807c801dd61a1224d
Steve Tipton <steve.tipton@employmentguide.com>
2012-05-16 15:40:20 -0400

    initialize Wildcat

--- lib/wildcat.rb
+++ lib/wildcat.rb
@@ -1,2 +1,15 @@
 module Wildcat
+  require 'yajl/json_gem'
+  require 'active_support/core_ext/hash'
+  require 'active_support/core_ext/object'
+  require 'active_model'
+  require 'typhoeus'
+
+  $LOAD_PATH.unshift(File.dirname(__FILE__))
+
+  require 'wildcat/version.rb'
+  require 'wildcat/config.rb'
+  require 'wildcat/team.rb'
+  require 'wildcat/game.rb'
+  require 'wildcat/error.rb'
 end
